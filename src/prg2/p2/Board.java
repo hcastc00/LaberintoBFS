78a68bf531ca252ff6ed86fd4d0d819204911609
hcastc00 <hcastc@estudiantes.unileon.es>
2019-10-20 13:21:36 +0200

    Implementada la lectura del tablero

---
+++ src/prg2/p2/Board.java
@@ -0,0 +1,45 @@
+package prg2.p2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Board {
+	
+	public int size;
+	public String[][] board;
+
+	public Board(int size) {
+		this.size = size;
+		this.board = new String [size][size];
+	}
+
+	public void readBoard() {
+		Scanner sc = new Scanner(System.in);
+		String input;
+		String[] inputArray = new String[this.size];
+		
+		for (int y = 0; y < this.size; y++) {
+			input = sc.nextLine();
+			inputArray = input.split("");
+			for (int x = 0; x < this.size; x++) {
+				this.board[y][x] = inputArray[x];
+			}
+			
+		}
+		
+		sc.close();
+	}
+
+    public void print2D() {
+        // Loop through all rows
+        for (String[] row : this.board) {
+
+            // converting each row as string
+            // and then printing in a separate line
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("\n");
+    }
+
+}
+
---
+++ src/prg2/p2/Main.java
@@ -0,0 +1,14 @@
+package prg2.p2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Board board = new Board(3);
+		board.readBoard();
+		board.print2D();
+	}
+
+	
+	
+	
+}
